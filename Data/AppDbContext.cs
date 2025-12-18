4a729c3a153719420be9fedf459a10b726daf05d
JarvisCamper <kharelramit@gmail.com>
2025-12-18 13:30:28 +0545

    Initial commit - Journal App with Entity Framework Core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JournalApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Maui.Storage;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JournalApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<JournalEntry> JournalEntries { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            string dbPath = Path.Combine(
+                FileSystem.AppDataDirectory,
+                "journal.db");
+            options.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
